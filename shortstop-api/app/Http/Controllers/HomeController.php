3a85122dd4120f986699a5debf5824232edcb764
Dale Moore <dmoore092@gmail.com>
2019-09-09 14:20:59 -0400

    initial commit

---
+++ shortstop-api/app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Players;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        $players = Players::all();
+        //$messages = Message::all();
+
+        foreach($players as $name){
+            echo $name->id;
+            echo "\r\n";
+        }die;
+
+        return view('home');
+    }
+}
---
+++ shortstop-api/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+@section('title', 'Athletic Prospects | Home')
+
+@section('content')
+
+    <h1>Content!</h1>
+
+@endsection
