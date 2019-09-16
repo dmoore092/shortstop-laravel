ca48ef41d898f04c92a947759f64bcdd48cd88e6
Dale Moore <dmoore092@gmail.com>
2019-09-16 16:40:56 -0400

    creating home page

--- shortstop-api/resources/views/home.blade.php
+++ shortstop-api/resources/views/home.blade.php
@@ -4,6 +4,33 @@
 
 @section('content')
 
-    <h1>Content</h1>
+<div id="body-main">
+    <div id="content">
+        <section>
+            <?php
+                try{
+                    $conn = mysqli_connect('127.0.0.1', 'root', 'y#GbqXtBGcy!z3Cf', 'shortstop');
+                    //echo "Connected successfully";
+                    $query = "SELECT header, text FROM about_us ORDER BY id DESC LIMIT 1;";
+                    $result = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo "<h2>".nl2br($row['header'])."</h2>";
+                        echo "<p>".nl2br($row['text'])."</p>";
+                        echo "<hr>";
+                    }
+                }
+                catch(exception $e){
+                    //echo "Connection failed: " . $e->getMessage();
+                }
+
+            ?>
+        </section>
+
+    </div><!-- end of #content -->
+    <div id="center-area">
+        <a href = "register.php"><img src = "/images/mainbanner.jpg" alt="main banner" /></a>
+    </div>
+
+
 
 @endsection
