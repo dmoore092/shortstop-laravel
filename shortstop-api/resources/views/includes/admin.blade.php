92e9ac0656b0eef187b1bc09c5df1de2214fa308
Dale Moore <dmoore092@gmail.com>
2019-09-25 16:47:42 -0400

    added button for downloading database

--- shortstop-api/resources/views/includes/admin.blade.php
+++ shortstop-api/resources/views/includes/admin.blade.php
@@ -1,14 +1,12 @@
 <h1 class="display-4">Admin Dashboard</h1>
+<a class="btn btn-secondary" href="">Download Database</a>
 <hr class="my-4">
 <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item">
-          <a class="nav-link active" id="blog-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Post A Blog</a>
+          <a class="nav-link active" id="blog-tab" data-toggle="tab" href="#blog" role="tab" aria-controls="home" aria-selected="true">Post A Blog</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" id="content-tab" data-toggle="tab" href="#content" role="tab" aria-controls="profile" aria-selected="false">Edit Site Content</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="contact" aria-selected="false">Edit A Player Profile</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" id="development-tab" data-toggle="tab" href="#development" role="tab" aria-controls="home" aria-selected="true">Pay For Web Development</a>
@@ -17,6 +15,5 @@
       <div class="tab-content" id="myTabContent">
         <div class="tab-pane fade show active" id="blog" role="tabpanel" aria-labelledby="home-tab">Post a blog</div>
         <div class="tab-pane fade" id="content" role="tabpanel" aria-labelledby="profile-tab">Edit site content</div>
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="contact-tab">edit player profile</div>
         <div class="tab-pane fade" id="development" role="tabpanel" aria-labelledby="contact-tab">pay for web development</div>
       </div>
