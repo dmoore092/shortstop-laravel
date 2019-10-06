396353571957b342b9df02121af916d55638579a
Dale Moore <dmoore092@gmail.com>
2019-10-05 20:08:01 -0400

    db details for heroku

--- shortstop-api/config/database.php
+++ shortstop-api/config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('mysql://i934dudnxi24ht9i:qbnaa4iga89ajsgo@wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/opjdazfdkmwv816r');
+
 return [
 
     /*
