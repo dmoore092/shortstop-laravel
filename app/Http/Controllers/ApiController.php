7dcb7404f63223829da1e1320e24ccd11b6cd186
Dale Moore <dwmdis@rit.edu>
2020-09-20 11:29:08 -0400

    testing an api route

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getHome() {
+        try{
+            //show homepage with page content from db
+            $post = HomeInfo::orderBy('created_at', 'desc')->limit(1)->get();
+            
+            /// shows homepage with blog content
+            $blogs = Blog::orderBy('created_at', 'desc')->paginate(3);
+
+            return view('pages.home')->with('post', $post[0])->with('blogs', $blogs);
+            // return view('pages.home')->with('posts', $posts);
+        }
+        catch(\Illuminate\Database\QueryException $ex){
+
+        }
+//        return dd($post[0]->header);
+        return response()->json([$post[0]]);
+        //view('pages.home')->with('post', $post[0]);
+      }
+}
