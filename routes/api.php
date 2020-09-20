eee9929878f3954d2ff84990ccd66e2000415723
Dale Moore <dwmdis@rit.edu>
2020-09-20 11:15:32 -0400

    testing an api route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'PagesController@index');
+// Route::get('/about', "PagesController@about");
+// Route::get('/conditioning', "PagesController@conditioning");
+// Route::get('/drills', "PagesController@drills");
+// Route::get('/recruiting', "PagesController@recruiting");
+// Route::get('/performance', "PagesController@performance");
+
+// Route::post('/updateHome', 'PagesController@updateHome')->middleware('admin');
+// Route::post('/updateAbout', 'PagesController@updateAbout')->middleware('admin');
+// // Route::get('/player', 'ProfileController@index');
+// //Route::get('/blog', 'PagesController@blog');
+
+// Route::resource('blog' ,'BlogController');
+// Route::resource('players' ,'PlayerController');
+
+// // Auth::routes();
+// Auth::routes(['verify' => true]);
+
+// Route::get('/dashboard', 'DashboardController@index');
