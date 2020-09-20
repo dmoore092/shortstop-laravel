6503d2498900d5d772e569b29f3e55a7843af412
Dale Moore <dwmdis@rit.edu>
2020-09-20 11:47:39 -0400

    figuring out api path

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,22 +7,22 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     public function index() {
-        return response()->json(["message" => "Hello from the API"]);
-        // try{
+        
+        try{
         //     //show homepage with page content from db
-        //     $post = HomeInfo::orderBy('created_at', 'desc')->limit(1)->get();
+            $post = HomeInfo::orderBy('created_at', 'desc')->limit(1)->get();
             
         //     /// shows homepage with blog content
         //     $blogs = Blog::orderBy('created_at', 'desc')->paginate(3);
 
         //     //return view('pages.home')->with('post', $post[0])->with('blogs', $blogs);
         //     // return view('pages.home')->with('posts', $posts);
-        // }
-        // catch(\Illuminate\Database\QueryException $ex){
+        }
+        catch(\Illuminate\Database\QueryException $ex){
 
-        // }
+        }
 //        return dd($post[0]->header);
-        
-        //view('pages.home')->with('post', $post[0]);
+        return response()->json(["message" => $post[0]]);
+        //view('pages.home')->with('post', );
       }
 }
