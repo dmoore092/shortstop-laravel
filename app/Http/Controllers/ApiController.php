0f2e0a076006cf907160a70400df6808c86f50bb
Dale Moore <dwmdis@rit.edu>
2020-09-20 12:12:21 -0400

    figuring out api path

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,10 +10,9 @@ class ApiController extends Controller
         //return response()->json(["message" => "hello from the api"]);
         try{
         //     //show homepage with page content from db
-            $post = HomeInfo::orderBy('created_at', 'desc')->limit(1)->get()->toJson(JSON_PRETTY_PRINT);;
-            //$post = [1, "cat"];
+            //$post = HomeInfo::orderBy('created_at', 'desc')->limit(1)->get()->toJson(JSON_PRETTY_PRINT);
         //     /// shows homepage with blog content
-        //     $blogs = Blog::orderBy('created_at', 'desc')->paginate(3);
+            $blogs = Blog::orderBy('created_at', 'desc')->paginate(3)->toJson(JSON_PRETTY_PRINT);
 
         //     //return view('pages.home')->with('post', $post[0])->with('blogs', $blogs);
         //     // return view('pages.home')->with('posts', $posts);
@@ -22,7 +21,7 @@ class ApiController extends Controller
 
         }
 //        return dd($post[0]->header);
-        return response($post, 200);
+        return response($blogs, 200);
         //view('pages.home')->with('post', );
       }
 }
