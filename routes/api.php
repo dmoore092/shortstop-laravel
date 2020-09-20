e9d706e6cd9a1e635d87ac1ce0310932398c0641
Dale Moore <dwmdis@rit.edu>
2020-09-20 11:22:53 -0400

    testing an api route

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', 'PagesController@index');
+Route::get('/home', 'PagesController@index');
 // Route::get('/about', "PagesController@about");
 // Route::get('/conditioning', "PagesController@conditioning");
 // Route::get('/drills', "PagesController@drills");
